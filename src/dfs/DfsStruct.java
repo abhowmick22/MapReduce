ca508ae85fad636b42115c94c33ca3ddeb3ffed1
Neil Dhruva <neildhruva@gmail.com>
2014-11-04 19:39:05 -0500

    adding to DFS

--- src/dfs/DfsStruct.java
+++ src/dfs/DfsStruct.java
@@ -8,26 +8,38 @@ package dfs;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DfsNode
+public class DfsStruct
 {
-    private String name;
-    private Map<String, DfsNode> subDirs;
+    private String name;                    //name of this node
+    private String path;                    //entire path of this node
+    private Map<String, DfsStruct> subDirs;
     private Map<String, DfsMetadata> files;
     
-    public DfsNode() {
-        this.subDirs = new HashMap<String, DfsNode>();
+    public DfsStruct(String name, String path) {
+        this.name = name;
+        this.path = path;
+        this.subDirs = new HashMap<String, DfsStruct>();
         this.files = new HashMap<String, DfsMetadata>();
+        
     }
     
     public String getName() {
         return this.name;
     }
     
+    public String getPath() {
+        return this.path;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
     
-    public Map<String, DfsNode> getSubDirsMap() {
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+    public Map<String, DfsStruct> getSubDirsMap() {
         return this.subDirs;
     }
     
